0983b514f1fd21ae6d87d86ad50f7a6128c7d395
Trey Richbourg <trey@allprosystems.com>
2020-05-14 14:54:06 -0500

    Organized Repo

--- myPets.py
+++
@@ -1,7 +0,0 @@
-myPets = ['Zophie', 'Pooka', 'Fat-tail']
-print('Enter a pet name:')
-name = input()
-if name not in myPets:
-    print(f'I do not have a pet name {name}')
-else:
-    print(f'{name} is my pet!')
