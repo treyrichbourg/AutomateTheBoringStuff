cd8486dc904b8acc317bfc98d9d1a15618b1d556
treyrichbourg <treyrichbourg@gmail.com>
2020-05-17 11:26:17 -0500

    5/17_morning_commit

--- Practice Projects/character_picture_grid.py
+++ Practice Projects/character_picture_grid.py
@@ -17,19 +17,20 @@ grid = [['.', '.', '.', '.', '.', '.'],
 # ...000...
 # ....0....
 
-for x in range(len(grid[0])):
-    for y in range(0, len(grid)):
-        if y < 8:
+
+for x in range(len(grid[0])): #loop through the current x values, will become our 'y' 
+    for y in range(0, len(grid)): #loop and grab values for what will become our 'x' 
+        if y < 8: #loops and grabs values 0 - 7 and prints in single line
             print(grid[y][x], end = '')
-        else:
+        else: #this will grab the item in value 8, return a line break, then go back to the original loop so we can get a new line
             print(grid[y][x])
 
-def heart():
-    for x in range(len(grid[0])):
-        for y in range(0, len(grid)):
-            if y < 8:
-                print(grid[y][x], end = '')
-            else:
-                print(grid[y][x])
+# def heart():
+#     for x in range(len(grid[0])):
+#         for y in range(0, len(grid)):
+#             if y < 8:
+#                 print(grid[y][x], end = '')
+#             else:
+#                 print(grid[y][x])
 
-print(heart())
+# print(heart())
