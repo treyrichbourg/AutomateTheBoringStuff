4ac22d68b8949047708781899c2dece6280d97b5
treyrichbourg <treyrichbourg@gmail.com>
2020-05-30 00:16:21 -0500

    solved Date Detection project

---
+++ Practice Projects/date_detection.py
@@ -0,0 +1,48 @@
+#! Python3
+# date_detection.py uses regex to detect dates in a DD/MM/YYYY format
+# stores them into variables named month, day and year 
+# validates if it is a valid date
+
+import re
+
+#Make one regex statement to catch the DD/MM/YYYY format
+date_regex = re.compile(r'''(
+    ([01]|[0-2][0-9]|3[01])  #capture the DD
+    (/)                      #/
+    ([0][1-9]|[1][0-2])      #MM
+    (/)                      #/
+    ([12]\d{3})              #YYYY
+)''', re.VERBOSE)         
+
+#Write a function to validate the input as an actual date
+def date_validator(text):
+    #Assign variables 
+    date = date_regex.search(text)
+    day, month, year = int(date.group(2)), int(date.group(4)), int(date.group(6))
+    months_with_30_days = [4,6,9,11]
+    feb = 2
+    leap = ''
+    #Check for leap year
+    if year % 4 == 0:
+        leap = year
+    elif year % 400 == 0:
+        leap = year
+    elif year % 100 == 0:
+        leap = 'no' 
+    if leap == year and month == feb:
+        if day > 29:
+            return 'This date is not valid!'
+    #Check months that only have 30 days
+    if month in months_with_30_days and day > 30:
+        return 'This date is not valid!'
+    #Everybody's favorite, February
+    if leap == 'no' and month == feb and day > 28:
+        return 'This date is not valid!'
+    
+    else:
+        print(date_regex.search(text).group())
+        return 'This is a valid date!'
+
+
+
+print(date_validator('Is today 26/06/1989?'))
