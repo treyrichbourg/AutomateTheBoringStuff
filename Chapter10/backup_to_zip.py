edd127528ea5fda1d66c3d8b0e7a90885f47de55
Trey Richbourg <trey@allprosystems.com>
2020-06-18 16:42:13 -0500

    backup_to_zip.py and selective_copy.py added

--- Chapter10/backup_to_zip.py
+++ Chapter10/backup_to_zip.py
@@ -8,12 +8,14 @@ def backup_to_zip(folder):
     #Back up the entire contents of "folder" into a ZIP file.
 
     folder = os.path.abspath(folder)   #make sure folder is absolute
+    print(folder)
 
     #Figure out the filename this code should use based on
     #what files already exist.
     number = 1
     while True:
         zip_filename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        #zip_filename = f"{os.path.basename(folder)}_{number}.zip"
         if not os.path.exists(zip_filename):
             break
         number += 1
@@ -22,8 +24,22 @@ def backup_to_zip(folder):
     print(f'Creating {zip_filename}...')
     backup_zip = zipfile.ZipFile(zip_filename, 'w')
 
-    #TODO: Walk the entire folder tree and compress the files in each folder.
-    print('Done.')
+    #Walk the entire folder tree and compress the files in each folder.
+    for foldername, subfolders, filenames in os.walk(folder):
+        print(f'Adding files in {foldername}...')
+        #Add the current folder to the ZIP file.
+        backup_zip.write(foldername)
 
-backup_to_zip()
+        #Add all the files in this folder to the ZIP file.
+        for filename in filenames:
+            new_base = os.path.basename(folder) + '_'
+            if filename.startswith(new_base) and filename.endswith('.zip'):
+                continue    #don't back up the backup ZIP files
+            backup_zip.write(os.path.join(foldername, filename))
+    backup_zip.close()
+
+
+    print('Done.')
+    
+backup_to_zip('/mnt/c/Users/trey/Documents/delicious')
 
---
+++ Practice Projects/selective_copy.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# selective-copy.py - Walk through a folder tree and search for files with a certain file extension.
+# Copy these files from whatever location they are in to a new folder.
+
+import os, shutil, re
+from pathlib import Path
+
+def selective_copy(folder, destination):
+    
+    #folder = Path(folder)
+    #destination = Path(destination)
+    extension = input('Enter a file extension to search for:\n')
+    #Walk through folder tree and grab all files that end with extension entered
+    for foldername, subfolders, filenames in os.walk(folder):
+        for filename in filenames:
+            if filename.endswith(extension):
+                #print(filename)
+                shutil.copy(os.path.join(foldername, filename), destination)  
+
+selective_copy('/mnt/c/Users/trey/Documents/delicious', '/mnt/c/Users/trey/Documents/test')
