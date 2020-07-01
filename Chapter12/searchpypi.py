cc286745521db44ef603f87c6a61f921d42e9a8b
treyrichbourg <treyrichbourg@gmail.com>
2020-07-01 01:01:20 -0500

    added requirements and searchpypi.py

--- Chapter12/searchpypi.py
+++ Chapter12/searchpypi.py
@@ -1,12 +1,19 @@
 #! /usr/bin/env python3
 # searchpypi.py - Opens several search results.
 
-import requests, sys, webbrowser, bs4 
+import requests, sys, webbrowser, bs4
 
-print('Searching...')   # Display text while downloading the search result page
-res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
+print("Searching...")  # Display text while downloading the search result page
+res = requests.get("https://pypi.org/search/?q=" + " ".join(sys.argv[1:]))
 res.raise_for_status()
 
-#TODO: Retrieve top search result links.
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text, "html.parser")
 
-#TODO: Open a browser tab for each result.
+# Open a browser tab for each result.
+link_elems = soup.select(".package-snippet")
+num_open = min(5, len(link_elems))
+for i in range(num_open):
+    url_to_open = f"https://pypi.org{link_elems[i].get('href')}"
+    print("Opening", url_to_open)
+    webbrowser.open(url_to_open)
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## Automate The Boring Stuff
 
-Here are some projects from my read-through of  ["Automate the Boring Stuff"](https://automatetheboringstuff.com/)  by Al Sweigart.
+Here are some projects from my read-through of  ["Automate the Boring Stuff"](https://automatetheboringstuff.com/)  by Al Sweigart.<br>
+Chapter projects are from the book with Al's solutions.  \
+Everything in the *Practice Projects* folder is my own solution.
 
+-Trey Richbourg
