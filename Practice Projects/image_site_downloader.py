d51630e90aaac498590cee374edb3df61155de8c
Trey Richbourg <trey@allprosystems.com>
2020-07-06 08:23:07 -0500

    quick edit

--- Practice Projects/image_site_downloader.py
+++ Practice Projects/image_site_downloader.py
@@ -6,17 +6,4 @@ import requests, bs4, os, praw
 import pyinputplus as pyip
 from pathlib import Path
 
-reddit = praw.Reddit(
-    client_id="6n8HaJ4i5jRaRA",
-    client_secret="VhTYE2fKDIkD575w4GTniJCm1vk",
-    redirect_uri="http://localhost:8080",
-    user_agent="wallpaper_downloader by u/Comet_D_Monkey",
-)
 
-reddit = praw.Reddit(
-    client_id="6n8HaJ4i5jRaRA",
-    client_secret="VhTYE2fKDIkD575w4GTniJCm1vk",
-    redirect_uri="http://localhost:8080",
-    user_agent="wallpaper_downloader by u/Comet_D_Monkey",
-)
-print(reddit.auth.url(["identity"], "...", "permanent"))
