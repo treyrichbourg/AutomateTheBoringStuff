df71d0db4f168fc565c1b21d57b67fcf87a4e115
treyrichbourg <treyrichbourg@gmail.com>
2020-07-08 23:54:30 -0500

    link_verification.py solved

--- Practice Projects/2048.py
+++ Practice Projects/2048.py
@@ -6,18 +6,20 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 def play_2048():
     browser = webdriver.Firefox()  # Open browser
     browser.get("https://play2048.co")  # Go to the website to play 2048
-    game_board = browser.find_element_by_css_selector('body')
+    game_board = browser.find_element_by_css_selector("body")
     while True:
         game_board.send_keys(Keys.UP)
-        time.sleep(.2)
+        time.sleep(0.2)
         game_board.send_keys(Keys.RIGHT)
-        time.sleep(.2)
+        time.sleep(0.2)
         game_board.send_keys(Keys.DOWN)
-        time.sleep(.2)
+        time.sleep(0.2)
         game_board.send_keys(Keys.LEFT)
-        time.sleep(.2)
+        time.sleep(0.2)
+
 
 play_2048()
--- Practice Projects/link_verification.py
+++ Practice Projects/link_verification.py
@@ -2,47 +2,59 @@
 # link_verification.py - Program that, given the URL of a web page, will attempt to download every linked page on the page.
 # The program will flag any pages that have 404 "Not Found" status code and print them out as broken links.
 
-import requests, bs4
-import pyinputplus as pyip
+import requests
+import bs4
 
-# Download every linked page on a given url
+# All in one solution to the problem in a single function
 def validate_links(url):
+    # Download url, check status, parse HTML
     res = requests.get(url)
     res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
     # Capture all <a href> tags and get the hyperlink
-    for link in soup.find_all('a'):
-        link_elem = link['href']
-        if 'https://' in link_elem:
+    for link in soup.find_all("a", href=True):
+        link_elem = link["href"]
+        if "http" in link_elem:
             new_response = requests.get(link_elem)
-            if new_response.status_code == 404:
-                print(f"Broken Link: {link_elem}")
+            status = new_response.status_code
+            if status == 404:
+                print(f"{res.url} returned a status code of {status} 'Not Found'.")
             else:
                 pass
-        else:
-            print(f"{link_elem}")
-            # new_response = requests.get(f"{url}/{link_elem}")
-            # if new_response.status_code == 404:
-            #     print(f"Broken Link: {link_elem}")
-            # else:
-            #     pass
     print(f"Checked all links in {url}")
-validate_links('https://www.python.org')
-# # Download every linked page on a given url
-# def get_link_elem(url):
-#     res = requests.get(url)
-#     res.raise_for_status()
-#     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-#     # Capture all <a href> tags and get the hyperlink
-#     for link in soup.find_all('a'):
-#         link_elem = link['href']
-#     return link_elem
 
-# def check_for_404(link):
-#     print(link)
 
-# def main(url):
-#     hyperlink = get_link_elem(url)
-#     check_for_404(hyperlink)
+# Problem broken down
+def capture_link_elem(url):
+    # Download url, check status, parse HTML, create list to hold links to other websites
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    website_links = []
+    # Capture all the link elements that lead to another website
+    for hyperlink in soup.find_all("a", href=True):
+        link_elem = hyperlink["href"]
+        if "http" in link_elem:
+            website_links.append(link_elem)
+    return website_links
 
-# main('https://www.python.org')
+
+def verify_link(list_of_links):
+    # Loop through every link in the list, grab status code and check for 404
+    for link in list_of_links:
+        res = requests.get(link)
+        status = res.status_code
+        if status == 404:
+            print(f"{res.url} returned a status code of {status} 'Not Found'.")
+        else:
+            pass
+
+
+def main(url):
+    list_of_links = capture_link_elem(url)
+    verify_link(list_of_links)
+    print("All links have been verified")
+
+
+main("https://www.python.org")
+
