240531e933edc15e2606d46142bce6dfcf9f4b18
treyrichbourg <treyrichbourg@gmail.com>
2020-07-15 01:04:24 -0500

    multiplication_table.py finish

--- Chapter13/update_produce.py
+++ Chapter13/update_produce.py
@@ -3,18 +3,17 @@
 
 import openpyxl
 
-wb = openpyxl.load_workbook('produceSales.xlsx')
-sheet = wb['Sheet']
+wb = openpyxl.load_workbook("produceSales.xlsx")
+sheet = wb["Sheet"]
 
 # The produce types and their updated prices
-PRICE_UPDATES = {'Garlic': 3.07,
-                 'Celery': 1.19,
-                 'Lemon': 1.27}
+PRICE_UPDATES = {"Garlic": 3.07, "Celery": 1.19, "Lemon": 1.27}
 
 # TODO: Loop through the rows and update the prices.
-for row_num in range(2, sheet.max_row): #skip the first row
+for row_num in range(2, sheet.max_row):  # skip the first row
     produce_name = sheet.cell(row=row_num, column=1).value
     if produce_name in PRICE_UPDATES:
         sheet.cell(row=row_num, column=2).value = PRICE_UPDATES[produce_name]
 
-wb.save('updated_produce_sales.xlsx')
+wb.save("updated_produce_sales.xlsx")
+
--- Practice Projects/multiplication_table.py
+++ Practice Projects/multiplication_table.py
@@ -6,18 +6,30 @@ from openpyxl.styles import Font
 import openpyxl
 import sys
 
-def create_table(n):
-    bold_font = Font(bold=True)
-    for i in range(1, n+1):
-        sheet[f"A{i+1}"].font = bold_font
-        sheet[f"A{i+1}"] = i
-        sheet.cell(row=1, column=i+1).font = bold_font
-        sheet.cell(row=1, column=i+1).value = i
 
-
-def main():
+def create_table():
     # Create Workbook and Worksheet objects, assign a variable to N, give sheet a title
     wb = openpyxl.Workbook()
     sheet = wb.active
-    n = sys.argv[1]
-    sheet.title = f'Multiplcation Table for {n}'
+    n = int(sys.argv[1])
+    sheet.title = f"Multiplcation Table for {n}"
+    bold_font = Font(bold=True)  # Make bold font type object
+    # Make row/col setup for the N*N table
+    for i in range(1, n + 1):
+        sheet[f"A{i+1}"].font = bold_font
+        sheet[f"A{i+1}"] = i
+        sheet.cell(row=1, column=i + 1).font = bold_font
+        sheet.cell(row=1, column=i + 1).value = i
+    # Insert function in each cell to do the math
+    for row in range(1, n + 1):
+        for col in range(1, n + 1):
+            _ = sheet.cell(row=row + 1, column=col + 1, value=f"={row}*{col}")
+    wb.save(f"Multiplication_table{n}.xlsx")
+
+
+def main():
+    create_table()
+
+
+if __name__ == "__main__":
+    main()
