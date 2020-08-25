c2364e13c352f07c6b868a4d61cc13687ca2ad5f
root <root@IT-Trey.allprosound.com>
2020-08-25 16:40:50 -0500

    stopwatch.py 'prettified'

--- Chapter17/stopwatch.py
+++ Chapter17/stopwatch.py
@@ -2,6 +2,7 @@
 # stopwatch.yp - A simple stopwatch program.
 
 import time
+import pyperclip
 
 # Display the program's instructions.
 print(
@@ -15,12 +16,16 @@ lap_num = 1
 
 try:
     while True:
+        output = []
         input()
         lap_time = round(time.time() - last_time, 2)
         total_time = round(time.time() - start_time, 2)
-        print(f"Lap {lap_num}: {total_time} ({lap_time})", end="")
+        output_str = f"Lap #{str(lap_num).rjust(2, ' ')}: {str(total_time).rjust(6, ' ')} ({str(lap_time).rjust(6, ' ')})"
+        print(output_str)
+        output.append(output_str)
         lap_num += 1
         last_time = time.time()
+        pyperclip.copy(output)
 except KeyboardInterrupt:
     # Handl ethe Ctrl-C exception to keep its error message from displaying.
     print("\nDone.")
