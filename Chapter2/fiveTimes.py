bf6ea9017c135a92e8f09c32598602594d008ee8
Trey Richbourg <trey@allprosystems.com>
2020-09-10 14:35:49 -0500

    Repo cleaned

--- Chapter2/fiveTimes.py
+++
@@ -1,9 +0,0 @@
-# print('My name is')
-# for i in range(5):
-#     print('Jimmy Five Times (' + str(i) + ')')
-
-print('My name is')
-i = 0
-while i < 5:
-    print('Jimmy Five Times (' + str(i) + ')')
-    i = i + 1
--- Chapter5/characterCount.py
+++
@@ -1,8 +0,0 @@
-message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
-count = {}
-#example of how setdefault method will add the key "character" to the dictionary 'count'
-for character in message:
-    count.setdefault(character, 0)
-    count[character] += 1
-
-print(count)
