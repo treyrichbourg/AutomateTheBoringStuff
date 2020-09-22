3cafa2ce24f176ab0b81f56055d80f35fa6fddb4
treyrichbourg <treyrichbourg@gmail.com>
2020-09-22 01:02:45 -0500

    sort_photo2 fix

--- sort_photo2.py
+++ sort_photo2.py
@@ -16,10 +16,9 @@ def argument_parser():
     parser = argparse.ArgumentParser(
         prog="sort_photo.py",
         usage="%(prog)s [path]",
-        description="Recursively search through a directory and sort photos based on date in exif data"
+        description="Recursively search through a directory and sort photos based on date in exif data",
     )
-    parser.add_argument(
-        "path", help="Enter the path you want to search through")
+    parser.add_argument("path", help="Enter the path you want to search through")
     args = parser.parse_args()
     return args
 
@@ -38,33 +37,40 @@ def get_exif(filename):
 
 
 def sort_jpgs(path):
-    for file in path.rglob('*.jpg' or '*.jpeg' or '*.JPG'):
+    for file in path.rglob("*.jpg" or "*.jpeg" or "*.JPG"):
         exif = get_exif(file)
         if exif == None:
             continue
-        exiftag = {ExifTags.TAGS[k]: v for k, v in exif.items(
-        ) if k in ExifTags.TAGS and type(v) is not bytes}
-        date_time_og = exiftag.get('DateTimeOriginal')
-        date_time = exiftag.get('DateTime')
+        exiftag = {
+            ExifTags.TAGS[k]: v
+            for k, v in exif.items()
+            if k in ExifTags.TAGS and type(v) is not bytes
+        }
+
+        # f = [exif.get(k) for k, v in ExifTags.TAGS.items() if v.startswith("Date")]
+
+        date_time_og = exiftag.get("DateTimeOriginal")
+        date_time = exiftag.get("DateTime")
         if date_time_og != None:
-            photo_date = re.match(
-                r"20\d\d", exiftag['DateTimeOriginal']).group()
+            photo_date = re.match(r"20\d\d", exiftag["DateTimeOriginal"]).group()
         elif date_time != None:
-            photo_date = re.match(r"20\d\d", exiftag['DateTime']).group()
+            photo_date = re.match(r"20\d\d", exiftag["DateTime"]).group()
         else:
             print(
-                f"'DateTimeOriginal' or 'DateTime' are not valid exif keys in:\n{file}")
+                f"'DateTimeOriginal' or 'DateTime' are not valid exif keys in:\n{file}"
+            )
             logging.debug(
-                f"'DateTimeOriginal' or 'DateTime' are not valid exif keys in:\n{file}")
-        Path(path/photo_date).mkdir(exist_ok=True)
+                f"'DateTimeOriginal' or 'DateTime' are not valid exif keys in:\n{file}"
+            )
+        Path(path / photo_date).mkdir(exist_ok=True)
         file_name = file.name
         source = Path(file)
-        dest = Path(path/photo_date)
-        #print(f"source = {source}")
+        dest = Path(path / photo_date)
+        # print(f"source = {source}")
         logging.info(f"source = {source}")
         logging.info(f"destination = {dest/file_name}")
-        #print(f"destination = {dest/file_name}")
-        if Path(dest/file_name).exists() == True:
+        # print(f"destination = {dest/file_name}")
+        if Path(dest / file_name).exists() == True:
             pass
         else:
             shutil.move(str(source), str(dest))
@@ -76,5 +82,5 @@ def main():
     sort_jpgs(p)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
